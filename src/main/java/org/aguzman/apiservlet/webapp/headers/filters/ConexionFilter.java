8bcd889b5b52d9c3e019bb882134bfb02bcc5a40
jart2222 <jart2607@gmail.com>
2022-12-15 19:10:30 -0600

    conexion y consulta a la base de datos

---
+++ src/main/java/org/aguzman/apiservlet/webapp/headers/filters/ConexionFilter.java
@@ -0,0 +1,39 @@
+package org.aguzman.apiservlet.webapp.headers.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import org.aguzman.apiservlet.webapp.headers.models.Producto;
+import org.aguzman.apiservlet.webapp.headers.services.ServiceJdbcException;
+import org.aguzman.apiservlet.webapp.headers.util.ConexionBaseDatos;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@WebFilter("/*")
+public class ConexionFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try(Connection conn= ConexionBaseDatos.getConnection()) {
+            if (conn.getAutoCommit()){
+                conn.setAutoCommit(false);
+            }
+            try {
+
+                request.setAttribute("conn", conn);
+                chain.doFilter(request,response);
+                conn.commit();
+            }catch (SQLException | ServiceJdbcException e){
+                conn.rollback();
+                ((HttpServletResponse) response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+                e.printStackTrace();
+            }
+
+        } catch (SQLException | ServiceJdbcException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/aguzman/apiservlet/webapp/headers/services/ProductoServiceJdbcImpl.java
@@ -0,0 +1,37 @@
+package org.aguzman.apiservlet.webapp.headers.services;
+
+import org.aguzman.apiservlet.webapp.headers.models.Carro;
+import org.aguzman.apiservlet.webapp.headers.models.Producto;
+import org.aguzman.apiservlet.webapp.headers.repositories.ProductoRepositoryJdbcImpl;
+import org.aguzman.apiservlet.webapp.headers.repositories.Repository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoServiceJdbcImpl implements ProductoService{
+    private ProductoRepositoryJdbcImpl repositoryJdbc;
+
+    public ProductoServiceJdbcImpl(Connection connection) {
+        this.repositoryJdbc = new ProductoRepositoryJdbcImpl(connection);
+    }
+
+    @Override
+    public List<Producto> listar() {
+        try {
+            return repositoryJdbc.listar();
+        } catch (SQLException e) {
+            throw new ServiceJdbcException(e.getMessage(), e.getCause());
+        }
+    }
+
+    @Override
+    public Optional<Producto> porId(Long id) {
+        try {
+            return Optional.ofNullable(repositoryJdbc.porId(id));
+        } catch (SQLException e) {
+            throw new ServiceJdbcException(e.getMessage(), e.getCause());
+        }
+    }
+}
