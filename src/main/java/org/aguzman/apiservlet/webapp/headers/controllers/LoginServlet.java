2e64cc6cd50eb4368da7f1185d5474474d2bb891
jart2222 <jart2607@gmail.com>
2023-01-16 21:57:17 -0600

    base de datos User

--- src/main/java/org/aguzman/apiservlet/webapp/headers/controllers/LoginServlet.java
+++ src/main/java/org/aguzman/apiservlet/webapp/headers/controllers/LoginServlet.java
@@ -3,18 +3,17 @@ package org.aguzman.apiservlet.webapp.headers.controllers;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
-import org.aguzman.apiservlet.webapp.headers.services.LoginService;
-import org.aguzman.apiservlet.webapp.headers.services.LoginServiceCookieImpl;
-import org.aguzman.apiservlet.webapp.headers.services.LoginServiceSessionImpl;
+import org.aguzman.apiservlet.webapp.headers.models.Usuario;
+import org.aguzman.apiservlet.webapp.headers.services.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
 import java.util.Optional;
 
 @WebServlet({"/login", "/login.html"})
 public class LoginServlet extends HttpServlet {
-    final static String USERNAME = "admin";
-    final static String PASSWORD = "12345";
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -49,7 +48,10 @@ public class LoginServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
-        if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+        UsuarioService service=new UsuarioServiceImpl((Connection) req.getAttribute("conn"));
+        Optional<Usuario> usuarioOptional=service.login(username,password);
+
+        if (usuarioOptional.isPresent()) {
 
             HttpSession session = req.getSession();
             session.setAttribute("username", username);
